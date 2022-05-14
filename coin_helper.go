015436ac438a25e9e0809003af1c5f712a224f27
mzgs <mzgsdev@gmail.com>
2022-05-14 11:38:25 +0300

    Update coin_helper.go

--- coin_helper.go
+++ coin_helper.go
@@ -93,7 +93,7 @@ func GetSourceCoin(symbol string) string {
 }
 
 func GetTargetCode(symbol string) string {
-	targets := []string{"TRY", "USDT", "BTC", "USD", "BNB", "TL"}
+	targets := []string{"TRY", "USDT", "BTC", "USD", "BNB", "TL","BUSD"}
 
 	for _, value := range targets {
 		if strings.HasSuffix(symbol, value) {
